043d491742acfa65cdbe442422c3755b5eaa4a12
Mazzberry <h.mohammadreza736@gmail.com>
2024-09-30 01:10:09 +0330

    List Comprehension

--- Part1/08.For_loops/For_loop11.py
+++ Part1/08.For_loops/For_loop11.py
@@ -5,4 +5,9 @@ for i in range(1,11):
         k =  i * j
         print(k ,end=' ')
     print()    
+
+#List Comprehension 
  
+x = [(k, str(k)) for k in range(50)]
+x = [k for k in range(50)]
+print(x) 
