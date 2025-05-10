7a6cd1cfa21bf4da8ad5d1315f5d0783193f81ae
Mazzberry <h.mohammadreza736@gmail.com>
2025-05-11 00:35:05 +0330

    Fix slicing syntax in St2.py for clarity

--- Part1/02.Strings/St2.py
+++ Part1/02.Strings/St2.py
@@ -3,7 +3,7 @@ a = 'Mohammadreza '
 b = 'hosseini'
 
 print(a[5])
-print(a[-1]) # az akhar
+print(a[-1:...]) # az akhar
 print(b[0:7])# slice range (b[ start : end ])
 print(a[:]+ b)
 print(a[0:13:2])# a[start : end : step]
