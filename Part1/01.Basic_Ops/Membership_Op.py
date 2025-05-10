cf6380f73ee4681506085585608459fe812276cd
Mazzberry <h.mohammadreza736@gmail.com>
2025-05-11 00:34:55 +0330

    Fix comment formatting for membership checks in Membership_Op.py

--- Part1/01.Basic_Ops/Membership_Op.py
+++ Part1/01.Basic_Ops/Membership_Op.py
@@ -1,6 +1,6 @@
-a = [1,2,3,4,5,6,7]
+a = [1,2,3,4,5,6,7] # عملیات عضویت
 
 print(1 in a)
-print(9 in a)
-print(1 not in a)
+print(9 in a) # aya 9 dar list a vojoud darad ?
+print(1 not in a) # aya 1 nist dar list a ?
 print(9 not in a )
