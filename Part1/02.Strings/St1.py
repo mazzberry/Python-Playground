40c1cc85576cdd99607a27f0e72b8acd370a6ed1
Mazzberry <h.mohammadreza736@gmail.com>
2025-05-11 00:35:00 +0330

    Improve comments for clarity and consistency in St1.py

--- Part1/02.Strings/St1.py
+++ Part1/02.Strings/St1.py
@@ -8,13 +8,17 @@ a = 'salam mn Mohammadam'
 b = "salam mn Mohammadam"
 #or 
 
-k = 'salam \t mn Mohammadam'
+k = 'salam \t mn Mohammadam' # \t 4 ta fasele mide
+
+# mesal kamel escape character ha : https://www.w3schools.com/python/gloss_python_escape_characters.asp
+
 #ـــــــــــــــــــــــــــــــــــــــــــــــــــــــ
 c = '''salam mn 
 Mohammad hastam'''
 #or 
 n = 'salam \n mn Mohammadreza hosseini hastam'
-m = r'salam \n mn Mohammadreza hosseini hastam' # real str : yani har chizi dakheleshe print kon
+m = r'salam \n mn Mohammadreza hosseini hastam' # real str : yani har chizi dar string vojud dare bedun
+#dar nazar gereftan Escape character ha print kon
 #[r] koja be kar mire?.  example :address
 address = r'c:\nima\tabriz\files'
  
